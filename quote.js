86d7147e7be884b86fbc51ccde3ab23c38f824e8
dtron500 <daniel.mitchell3005@gmail.com>
2024-07-25 14:10:31 +0100

    Ticket 2c

--- quote.js
+++ quote.js
@@ -6,19 +6,30 @@ const fileName = "quotes.json";
 export async function getQuotes() {
     try {
         const data = await fs.readFile(fileName, 'utf8');
-        const quotes = json.parse (data);
-        return quotes; 
+        return JSON.parse(data);
     } catch (error) {
         console.error('Error reading file:', error);
+        return[];
     }
 }
 
 
 export async function addQuote(quoteText) {
+    const quotes = await getQuotes();
+    const newQuote = {
+        id: uuidv4(),
+        quoteText,
+    };
+    quotes.push(newQuote);
+    await fs.writeFile(fileName, JSON.stringify(quotes, null, 2));
+    return newQuote;
 
 }
 
 export async function getRandomQuote() {
+    const data = await fs.readFile(fileName, 'utf8');
+const quotes = JSON.parse(data);
+
 
 }
 
