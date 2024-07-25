651b4a1188c1d1c666c44b00b99830816a715f31
Connie <connieramirezphotography@gmail.com>
2024-07-25 12:34:18 +0100

    Ticket 2A

--- quote.js
+++ quote.js
@@ -4,8 +4,15 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-
+    try {
+        const data = await fs.readFile(fileName, 'utf8');
+        const quotes = json.parse (data);
+        return quotes; 
+    } catch (error) {
+        console.error('Error reading file:', error);
+    }
 }
+
 
 export async function addQuote(quoteText) {
 
